22dbe5fe5d8039a1073303d1154e70d2b6206a67
parcial_1 <00064815@uca.edu.sv>
2017-06-04 21:01:22 -0600

    c:

--- app/src/main/java/com/delao00064815/copproyecto/MainActivity.java
+++ app/src/main/java/com/delao00064815/copproyecto/MainActivity.java
@@ -2,6 +2,10 @@ package com.delao00064815.copproyecto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int res_id = item.getItemId();
+        if(res_id==R.id.register){
+            Toast.makeText(getApplicationContext(),"Aun no funca pero eh :v",Toast.LENGTH_LONG).show();
+        }
+        /*else if(){
+
+        }*/
+        return true;
     }
 }
---
+++ app/src/main/java/com/delao00064815/copproyecto/Registro.java
@@ -0,0 +1,8 @@
+package com.delao00064815.copproyecto;
+
+/**
+ * Created by rober on 4/6/2017.
+ */
+
+public class Registro {
+}
