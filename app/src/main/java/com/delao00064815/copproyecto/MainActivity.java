c85d2b6e450445d005cc5248e573502160918578
parcial_1 <00064815@uca.edu.sv>
2017-06-04 19:24:55 -0600

    Initial commit

---
+++ app/src/main/java/com/delao00064815/copproyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.delao00064815.copproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
