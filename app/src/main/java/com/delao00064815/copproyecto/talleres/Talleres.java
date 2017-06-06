e197d7d3e433204d7ece14a384f9bf72679ad82f
hmmatus <manriquematus1@gmail.com>
2017-06-05 21:16:19 -0600

    Commit messages

---
+++ app/src/main/java/com/delao00064815/copproyecto/talleres/Talleres.java
@@ -0,0 +1,10 @@
+package com.delao00064815.copproyecto.talleres;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hmanr on 5/6/2017.
+ */
+
+public class Talleres extends AppCompatActivity {
+}
