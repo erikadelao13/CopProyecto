591b13e9f6b77c62cf9089fe59c36eb853d3f977
DYuusha <cesarlopezlima1@gmail.com>
2017-06-05 20:41:11 -0600

    miparte

---
+++ app/src/main/java/com/delao00064815/copproyecto/designs/TrabajosPasan.java
@@ -0,0 +1,15 @@
+package com.delao00064815.copproyecto.designs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.delao00064815.copproyecto.R;
+
+public class TrabajosPasan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabajos_pasan);
+    }
+}
