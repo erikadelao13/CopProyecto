1552e74a3fc66de66a7fc68d070c5a6d8daf194d
DYuusha <cesarlopezlima1@gmail.com>
2017-06-25 17:20:16 -0600

    barradebuscarbonita(prueba)

--- app/src/main/java/com/delao00064815/copproyecto/ofertaEmpleo/OfertaEmpleo.java
+++ app/src/main/java/com/delao00064815/copproyecto/ofertaEmpleo/OfertaEmpleo.java
@@ -1,34 +1,93 @@
 package com.delao00064815.copproyecto.ofertaEmpleo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.delao00064815.copproyecto.Login;
 import com.delao00064815.copproyecto.R;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hmanr on 5/6/2017.
  */
 
 public class OfertaEmpleo extends AppCompatActivity {
-
+    MaterialSearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ofertas_empleo);
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+       // getSupportActionBar().setTitle("Material Search");
+       toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+                //If closed Search View , lstView will return default
+                //lstView = (ListView)findViewById(R.id.lstView);
+                //ArrayAdapter adapter = new ArrayAdapter(OfertaEmpleo.this,android.R.layout.simple_list_item_1,lstSource);
+               // lstView.setAdapter(adapter);
+
+            }
+        });
+
+        /*searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText != null && !newText.isEmpty()){
+                    //List<String> lstFound = new ArrayList<String>();
+                    for(String item:lstSource){
+                        if(item.contains(newText))
+                            lstFound.add(item);
+                    }
+
+                   ArrayAdapter adapter = new ArrayAdapter(OfertaEmpleo.this,android.R.layout.simple_list_item_1,lstFound);
+                    lstView.setAdapter(adapter);
+                }
+                else{
+                    //if search text is null
+                    //return default
+                    ArrayAdapter adapter = new ArrayAdapter(OfertaEmpleo.this,android.R.layout.simple_list_item_1,lstSource);
+                    lstView.setAdapter(adapter);
+                }
+                return true;
+            }
+
+        });*/
     }
-    @Override
+
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu,menu);
         return true;
     }
-
+*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int res_id = item.getItemId();
@@ -39,6 +98,12 @@ public class OfertaEmpleo extends AppCompatActivity {
         /*else if(){
 
         }*/
+        return true;
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
         return true;
     }
 }
